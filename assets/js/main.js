5d5c6fa026e5e24552d90d77a845209bdb466172
Alfredo Goytia <fredo.goytia@gmail.com>
2015-09-09 07:34:22 -0500

    Adding support for launching netflix app from google chrome
    google chrome changed the way they launch mobile applications and this
    is the new structure.

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,8 +177,7 @@ var nflxBtn = document.getElementById("nflxBtn");
 
 function nflxBtnClick()
 {
-   window.location("nflx://www.netflix.com/");
-    alert('Button has been pushed');
+    window.open("intent://www.netflix.com/#Intent;scheme=nflx;package=com.netflix.mediclient;S.browser_fallback_url=http%3A%2F%2Fwww.netflix.com;end", "_blank");
 }
 
 nflxBtn.addEventListener('click', nflxBtnClick, false);
